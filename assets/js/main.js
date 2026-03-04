da7461c2fefc973aa311557ec4415a323efdbd04
PaoBip <paolacaceresgeisse2016@gmail.com>
2026-03-04 23:52:44 +0000

    se agrego boton de smooth scrolling

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const boton = document.getElementById("btnMimitos");
+const alerta = document.getElementById("alertaMimitos");
+
+boton.addEventListener("click", function() {
+  alerta.classList.remove("d-none");
+  setTimeout(function() {
+    alerta.classList.add("d-none");
+  }, 3000);
+});
+
+const botonArriba = document.getElementById("btn-arriba");
+
+botonArriba.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
